ff3c3a0329ac0c0964ecc2cc2c6462ffe6f56a93
hn_ryanb <13665741+rb1193@users.noreply.github.com>
2024-10-15 16:35:15 +0100

    Make sure vite config isn't broken during prod build, where SSL cert is not provided

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,24 @@ import react from '@vitejs/plugin-react-swc'
 import fs from 'fs'
 import path from 'path'
 
+let cert;
+let key;
+
+try {
+  cert = fs.readFileSync(path.resolve(__dirname, 'certs/server.pem'))
+  key = fs.readFileSync(path.resolve(__dirname, 'certs/key.pem'))
+} catch {
+  console.log("No SSL certificate provided, so CORS will not work locally")
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
   server: {
     https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs/server.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem')),
+      cert,
+      key,
     },
     host: 'local.recipes.arbee.me',
     port: 3000
