72e5ba6e47f16b6467a57a6f60c73234dd547c34
hn_ryanb <13665741+rb1193@users.noreply.github.com>
2024-10-15 15:06:33 +0100

    Remove unused cypress and miragejs libs

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import bootCypress from './bootCypress';
 import theme from './theme';
-
-bootCypress()
 
 const rootEl: Element = document.getElementById('root') as Element;
 const root = createRoot(rootEl);
@@ -18,8 +14,3 @@ root.render(<BrowserRouter>
         <App />
     </ChakraProvider>
 </BrowserRouter>)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
